20af5333e81b984de1fc0990e08926a0881c7434
tsiozos <38419287+tsiozos@users.noreply.github.com>
2020-11-09 12:16:25 +0200

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,91 @@
+stationID = 0
+basic.show_number(0)
+stationACK = range(26).fill(0)
+
+##### SETUP #####
+def on_button_pressed_b():
+    global stationID
+    stationID = (stationID + 1) % 26
+    if stationID > 9:
+        drawNumber(stationID)
+    else:
+        basic.show_number(stationID)
+    pass
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+##### SERVER #####
+##### SEND SYNC REQ #####
+
+def on_button_pressed_ab():
+    global stationID, stationACK
+    #ASKING FOR SYNC
+    if stationID == 0:
+        for i in range(1,26):
+            for j in range(10):
+                if stationACK[i] == 0:    #if station didn't reply yet keep sending
+                    radio.send_value("SYNC", 0)
+                    basic.pause(100)
+            drawNumber(i)
+        basic.clear_screen()
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+### DUMP THE stationACKs
+def on_button_pressed_a():
+    global stationACK
+    lack = len(stationACK)
+    for i in range(lack):
+        print("Station "+str(i)+" ACK: "+ str(Math.map(stationACK[i],0,255,-128,-42)))
+        basic.pause(100)
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+##### CLIENT ACCEPTING REQ #####
+def on_received_value(name, value):
+    global stationID, stationACK
+    if stationID == 0:    #if we are the server then accept the ACK command
+        if name == "ACK":
+            if value > 0 & value <= 25:
+                stationACK[value] = Math.map(getRSSI(),-128,-42,0,255)
+                print("station "+str(value)+" has RSSI: "+getRSSI()+" ("+stationACK[value]+")")
+    else:
+        if name=="SYNC":    #if we are a client we send ACK several times
+            tries = triesFromRSSI(getRSSI(),0.95,9)
+            print("sending ACK "+str(tries)+" times")
+            if value == stationID:      ### REPLY ONLY IF WE HAVE A MATCHING STATIONID
+                for i in range(tries):  ### tries ARE CALC'd WITH 95% RELIABILITY TARGET
+                    radio.send_value("ACK",stationID)
+                    basic.show_number(i)
+                    basic.pause(randint(1, 10)*200)
+                basic.clear_screen()
+radio.on_received_value(on_received_value)
+
+
+##### UTILITIES #####
+##### DRAW A NUMBER WITH LEDS #####
+def drawNumber(n: number):
+    basic.clear_screen()
+    if n>=0 & n<=25:
+        for i in range(n):
+            led.plot(i % 5, i // 5)
+    else:
+        basic.show_icon(IconNames.SAD)
+
+def getRSSI():
+    return radio.received_packet(RadioPacketProperty.SIGNAL_STRENGTH)
+
+def triesN(y,p):
+    return Math.ceil(Math.log(1-y)/Math.log(p))
+
+def lossP(y,n):
+    return Math.pow((1-y),1/n)
+
+def triesFromRSSI(rssi: float, y:float, maxtries: int):
+    rssi2 = rssi + 100
+    p = Math.min(1,5936.2673*rssi2**(-3.7231)) # this function may return a p > 1
+    # so we limit it to 1
+    if p==1:
+        t = maxtries
+    else:
+        t = Math.max(1,triesN(y,p))  #if tries fall below 1, at least 1 try
+    return t
+
+#drawNumber(19)
---
+++ main.ts
@@ -0,0 +1,114 @@
+let stationID = 0
+basic.showNumber(0)
+let stationACK = _py.range(26).fill(0)
+// #### SETUP #####
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
+    
+    stationID = (stationID + 1) % 26
+    if (stationID > 9) {
+        drawNumber(stationID)
+    } else {
+        basic.showNumber(stationID)
+    }
+    
+    
+})
+// #### SERVER #####
+// #### SEND SYNC REQ #####
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
+    // ASKING FOR SYNC
+    if (stationID == 0) {
+        for (let i = 1; i < 26; i++) {
+            for (let j = 0; j < 10; j++) {
+                if (stationACK[i] == 0) {
+                    // if station didn't reply yet keep sending
+                    radio.sendValue("SYNC", 0)
+                    basic.pause(100)
+                }
+                
+            }
+            drawNumber(i)
+        }
+        basic.clearScreen()
+    }
+    
+})
+// ## DUMP THE stationACKs
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    
+})
+// #### CLIENT ACCEPTING REQ #####
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let tries: number;
+    
+    if (stationID == 0) {
+        // if we are the server then accept the ACK command
+        if (name == "ACK") {
+            if (value > (0 & value) && (0 & value) <= 25) {
+                stationACK[value] = Math.map(getRSSI(), -128, -42, 0, 255)
+                console.log("station " + ("" + value) + " has RSSI: " + getRSSI() + " (" + stationACK[value] + ")")
+            }
+            
+        }
+        
+    } else if (name == "SYNC") {
+        // if we are a client we send ACK several times
+        tries = triesFromRSSI(getRSSI(), 0.95, 9)
+        console.log("sending ACK " + ("" + tries) + " times")
+        if (value == stationID) {
+            // ## REPLY ONLY IF WE HAVE A MATCHING STATIONID
+            for (let i = 0; i < tries; i++) {
+                // ## tries ARE CALC'd WITH 95% RELIABILITY TARGET
+                radio.sendValue("ACK", stationID)
+                basic.showNumber(i)
+                basic.pause(randint(1, 10) * 200)
+            }
+            basic.clearScreen()
+        }
+        
+    }
+    
+})
+// #### UTILITIES #####
+// #### DRAW A NUMBER WITH LEDS #####
+function drawNumber(n: number) {
+    basic.clearScreen()
+    if (n >= (0 & n) && (0 & n) <= 25) {
+        for (let i = 0; i < n; i++) {
+            led.plot(i % 5, Math.idiv(i, 5))
+        }
+    } else {
+        basic.showIcon(IconNames.Sad)
+    }
+    
+}
+
+function getRSSI(): number {
+    return radio.receivedPacket(RadioPacketProperty.SignalStrength)
+}
+
+function triesN(y: number, p: number): number {
+    return Math.ceil(Math.log(1 - y) / Math.log(p))
+}
+
+function lossP(y: number, n: number): number {
+    return Math.pow(1 - y, 1 / n)
+}
+
+function triesFromRSSI(rssi: number, y: number, maxtries: number): number {
+    let t: number;
+    let rssi2 = rssi + 100
+    let p = Math.min(1, 5936.2673 * rssi2 ** -3.7231)
+    //  this function may return a p > 1
+    //  so we limit it to 1
+    if (p == 1) {
+        t = maxtries
+    } else {
+        t = Math.max(1, triesN(y, p))
+    }
+    
+    // if tries fall below 1, at least 1 try
+    return t
+}
+
